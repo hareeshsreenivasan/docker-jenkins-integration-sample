dcce3b5ddc4fbdaecc7598933e7363b165e532cf
Hareesh <trivandrum50@gmail.com>
2022-12-28 19:40:45 +0530

    second commit

---
+++ src/main/java/com/javatechie/Employee.java
@@ -0,0 +1,31 @@
+package com.javatechie;
+
+public class Employee {
+    private String fName;
+    private String sName;
+    private int age;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
